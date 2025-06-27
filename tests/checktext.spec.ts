7c6e9b30cabc81b2b2f579945697bbfbaf452301
KC0122 <sacakameel@yahoo.co.uk>
2025-06-27 23:59:07 +0100

    Refactored code

--- tests/checktext.spec.ts
+++ tests/checktext.spec.ts
@@ -1,20 +1,15 @@
 import { test, expect } from '@playwright/test';
-/*
+
 test('has title', async ({ page }) => {
   await page.goto('https://www.bbc.co.uk/');
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/BBC - Home/);
 });
-*/
+
 
 test('Upload a text file to QA-Automation site', async ({ page }) => {
   await page.goto('https://qa-automation-practice.netlify.app/file-upload.html');
   await page.locator('input[type="file"]').setInputFiles('./tests/Txt.txt');
   await page.locator('button[type="submit"]').click();
   await expect(page.locator('div#file_upload_response')).toHaveText(/successfully uploaded/);
-  // Click the get started link.
-
-  // Click the get started link.
-  //await page.getByRole('link', { name: 'Sign in' }).click();
-
-});
+  });
