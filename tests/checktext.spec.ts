76d243ce4fbe09237d06229748178ad3f60954b0
KC0122 <sacakameel@yahoo.co.uk>
2025-06-29 13:11:31 +0100

    Updated two fileupload tests

--- tests/checktext.spec.ts
+++ tests/checktext.spec.ts
@@ -1,15 +1,20 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://www.bbc.co.uk/');
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/BBC - Home/);
+test('Upload via filechooser method', async ({ page }) => {
+  await page.goto('https://practice.expandtesting.com/upload');
+  const filechooserpromise=  page.waitForEvent('filechooser');
+  await page.locator('input#fileInput').click();
+  const choosefile = await filechooserpromise;
+  await choosefile.setFiles('./tests/upload/Txt.txt');
+  await page.waitForTimeout(6000);
+  await page.locator('button#fileSubmit').click();
+  await expect(page.locator('h1')).toHaveText(/File Uploaded!/);    
 });
 
 
-test('Upload a text file to QA-Automation site', async ({ page }) => {
+test('Upload a text file using Input method ', async ({ page }) => {
   await page.goto('https://qa-automation-practice.netlify.app/file-upload.html');
-  await page.locator('input[type="file"]').setInputFiles('./tests/Txt.txt');
+  await page.locator('input[type="file"]').setInputFiles('./tests/upload/Txt.txt');
   await page.locator('button[type="submit"]').click();
   await expect(page.locator('div#file_upload_response')).toHaveText(/successfully uploaded/);
   });
